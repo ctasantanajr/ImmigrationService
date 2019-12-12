d7edf9ba5af8d0e1aeb356210f77995c8fb9fa77
ctasantanajr <33551129+ctasantanajr@users.noreply.github.com>
2019-12-12 23:08:30 +0000

    modified Main.java -> Menu + print all queue

--- Main.java
+++ Main.java
@@ -3,44 +3,156 @@
  */
 package assignment;
 
+import java.util.Scanner;
+
 /**
  * @author Claudio Santana - Student nr. 2017180
  *
  */
 public class Main {
-	
-	Main (){
-		
+
+	static Scanner sc = new Scanner(System.in);
+	static Boolean run = true;
+
+	Main() {
+
 		DLL myList = new DLL();
-		
+
 		Person p1 = new Person(1, "Claudio", "Santana", 12122019, "FHG1234", PriorityLevel.HIGH);
 		Node n1 = new Node(p1);
-		
+
 		Person p2 = new Person(2, "Joice", "Scarabeli", 12122019, "FZZ2211", PriorityLevel.HIGH);
 		Node n2 = new Node(p2);
-		
+
 		Person p3 = new Person(3, "Neusa", "Shan", 12122019, "JJK2785", PriorityLevel.HIGH);
 		Node n3 = new Node(p3);
-		
-		
+
 		myList.addTail(p1);
 		myList.addTail(p2);
 		myList.addTail(p3);
-		
-		System.out.println(myList.getHead().getData());
-		System.out.println(myList.getTail().getData());
-		System.out.println(myList.getSize());
-		
-		System.out.println("-------------------------------------------------");
-		
-		Person test = myList.removeHead();
-		
-		System.out.println("The person served was "+test);
-		System.out.println(myList.getHead().getData());
-		System.out.println(myList.getTail().getData());
-		System.out.println(myList.getSize());
-		
-		
+
+		/*
+		 * System.out.println(myList.getHead().getData());
+		 * System.out.println(myList.getTail().getData());
+		 * System.out.println(myList.getSize());
+		 * 
+		 * System.out.println("-------------------------------------------------");
+		 * 
+		 * Person test = myList.removeHead();
+		 * 
+		 * System.out.println("The person served was "+test);
+		 * System.out.println(myList.getHead().getData());
+		 * System.out.println(myList.getTail().getData());
+		 * System.out.println(myList.getSize());
+		 */
+
+		Welcome();
+
+		// this while loop builds up the menu of the system, utilising the I/O class
+		// scanner to receive the input from the user
+		while (run) {
+
+			System.out.println("**********************************************************************");
+			System.out.println("              Please Select From The Following Options:               ");
+			System.out.println("**********************************************************************");
+
+			System.out.println("Enter 1 to list the queue");
+			System.out.println("Enter 2 to add a person to the queue");
+			System.out.println("Enter 3 to remove a person from the queue");
+			System.out.println("Enter 4 to search the position of a person in the queue");
+			System.out.println("Enter 5 to load Immigration Service Appointment records");
+			System.out.println("Enter 6 to save Immigration Service Appointment records");
+			System.out.println("Enter 7 to quit without saving");
+
+			int answer = sc.nextInt();
+
+			// this conditional switch executes blocks of methods in regards to the option
+			// typed by the user. After executing the whole block of commands it breaks and
+			// print off the option menu again
+			switch (answer) {
+
+			case 1:
+				printlist(myList.getHead());
+				break;
+
+			case 2:
+				// MenuAddSelect();
+				break;
+
+			case 3:
+				// MenuSearchTitle();
+				break;
+
+			case 4:
+				
+				break;
+
+			case 5:
+				// loadFiles();
+				break;
+
+			case 6:
+				/*
+				 * saveMusicRecord(); saveLiveConcertRecord(); saveMovieRecord();
+				 * saveBoxSetRecord();
+				 */
+				System.out.println("Thanks for using Immigration Service Appointment System!");
+				break;
+
+			case 7:
+				EndProgram();
+				break;
+
+			}
+		}
+		System.exit(0);
+
+	}
+	
+	
+	// ------------------------------------------------------------------
+	// METHOD TO PRINT ALL THE QUEUE
+	// ------------------------------------------------------------------
+	
+	// This function prints contents of linked list starting from the first node 
+    public void printlist(Node node) 
+    { 
+        Node last = null; 
+        System.out.println("List of people in the queue:"); 
+        while (node != null) { 
+            System.out.println(node.getData().toString() + " "); 
+            last = node; 
+            node = node.getNext(); 
+        } 
+        /*System.out.println(); 
+        System.out.println("Traversal in reverse direction"); 
+        while (last != null) { 
+            System.out.print(last.getData().toString() + " "); 
+            last = last.getPrevious(); 
+        }*/ 
+    } 
+	
+
+	// --------------------------------------------------------------------
+	// METHOD TO WELCOME USER
+	// --------------------------------------------------------------------
+
+	private static void Welcome() {
+		// TODO Auto-generated method stub
+		System.out.println("\nWelcome to Immigration Service Appointment System!");
+		System.out.println();
+
+	}
+
+	// ------------------------------------------------------------------
+	// ENDING THE PROGRAM
+	// ------------------------------------------------------------------
+
+	public static void EndProgram() {
+
+		System.out.println("Thanks for using Immigration Service Appointment System!");
+		System.exit(0);
+
 	}
 
 	/**
