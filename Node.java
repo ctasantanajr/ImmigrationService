9ce7083199192332a6ff16f14a8434182b677d3c
ctasantanajr <33551129+ctasantanajr@users.noreply.github.com>
2019-12-12 13:38:05 +0000

    creating class Person + modifying class Node

--- Node.java
+++ Node.java
@@ -1,5 +1,64 @@
+/**
+ * 
+ */
 package assignment;
 
+/**
+ * @author Claudio Santana - Student nr. 2017180
+ *
+ */
 public class Node {
+	
+	private Node next;
+	private Node previous;
+	private Person data;
+	/**
+	 * @param next
+	 * @param previous
+	 * @param data
+	 */
+	private Node(Node next, Node previous, Person data) {
+		this.next = next;
+		this.previous = previous;
+		this.data = data;
+	}
+	/**
+	 * @return the next
+	 */
+	public Node getNext() {
+		return next;
+	}
+	/**
+	 * @param next the next to set
+	 */
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	/**
+	 * @return the previous
+	 */
+	public Node getPrevious() {
+		return previous;
+	}
+	/**
+	 * @param previous the previous to set
+	 */
+	public void setPrevious(Node previous) {
+		this.previous = previous;
+	}
+	/**
+	 * @return the data
+	 */
+	public Person getData() {
+		return data;
+	}
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(Person data) {
+		this.data = data;
+	}
+	
+	
 
 }
---
+++ Person.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package assignment;
+
+/**
+ * @author Claudio Santana - Student nr. 2017180
+ *
+ */
+public class Person {
+
+}
